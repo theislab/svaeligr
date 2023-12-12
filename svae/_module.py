5655e7417738d2df4b841ab00c5d6f7af2468dd5
tomf97 <tom_fischer1997@gmx.de>
2023-12-12 15:00:32 +0100

    remove softmax

--- svae/_module.py
+++ svae/_module.py
@@ -317,7 +317,6 @@ class SpikeSlabVAEModule(BaseModuleClass):
         # prune out entries according to mask
         mean_z_pruned = mean_z*mask
         #order labels
-        #w = torch.nn.functional.softmax(self.w, dim=0)
         w = torch.sigmoid(self.w)
         w = w.squeeze()
         batch_size = mean_z_pruned.shape[0]
@@ -475,9 +474,7 @@ class SpikeSlabVAEModule(BaseModuleClass):
 
         # mixture weight prior
         prior_mw = torch.ones_like(self.w)
-
         w_discrete =  torch.sigmoid(self.w) # torch.nn.functional.softmax(self.w, dim=0)
-
         logp_mw = (
             torch.distributions.Beta(prior_mw, prior_mw*self.sparse_mask_penalty)
             .log_prob(w_discrete)
